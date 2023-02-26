90e20cf3f9f042675f8a2fe9f154430ea1324850
Danya <danil_timchenko_1997@mail.ru>
2023-02-26 15:22:31 +0300

    Create Base Of Bot
    Элементарная основа бота с приветствием

---
+++ test_bot/Program.cs
@@ -0,0 +1,95 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace bot_Application
+{
+    class _MainProgramm
+    {
+        private static bool isHello = false, isYesOrNo = false, isTypingInfo = false;
+        private static void Main(string[] args)
+        {
+            TelegramBotClient bot = new TelegramBotClient("6218350946:AAHR0Z4Z5M36BNVmkGEmfdU__JA_CKk3Jgs");
+            bot.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+        async private static Task Update(ITelegramBotClient client, Update upd, CancellationToken token)
+        {
+            if(upd.Message.Text != null )
+            {      
+                if(isTypingInfo)
+                {
+                    string info = upd.Message.Text;
+                    /*Проверка корректности ввода данных от пользователя*/
+                }
+                if (isHello && isYesOrNo)
+                {
+                    await client.SendTextMessageAsync(
+                            upd.Message.Chat.Id, 
+                            "Не совсем вас понял, но вот что можно сделать", 
+                            replyMarkup: replyMainMenuWithYesKeyboardMarkup);
+                }
+                if(isYesOrNo)
+                {
+                    return;
+                }
+                if(upd.Message.Text == "ДА")
+                {
+                    isHello = true;
+                    isYesOrNo = true;
+                        await client.SendTextMessageAsync(
+                            upd.Message.Chat.Id, 
+                            "Что интересует?", 
+                            replyMarkup: replyMainMenuWithYesKeyboardMarkup
+                        );
+                    /*Проверка анкеты*/
+                }
+                else if(upd.Message.Text == "НЕТ")
+                {
+                    isHello = true;
+                        await client.SendTextMessageAsync(
+                            upd.Message.Chat.Id, 
+                            "Ничего страшного, сейчас вместе создадим вашу анкету!\nОдним сообщением введите следующую информацию:\n1. Фамилия Имя\n2. Адрес электронной почты\n3. Номер телефона\n!!!ОБЯЗАТЕЛЬНО!!! каждый пункт заполняйте на новой строке но в одном сообщении!",
+                            replyMarkup: new ReplyKeyboardRemove()
+                        );
+                }
+                if(isHello)
+                {
+                    return;
+                }
+                await client.SendTextMessageAsync(
+                    upd.Message.Chat.Id, 
+                    "Вас приветствует бот для поиска полезных контактов! У вас уже есть анкета?", 
+                    replyMarkup: replyMainMenuWithNoOrYesKeyboardMarkup);   
+
+            }
+        }
+        private static bool CheckForm() /*КУСОК В РАЗРАБОТКЕ*/
+        {
+            return true;
+        }
+        private static ReplyKeyboardMarkup replyMainMenuWithYesKeyboardMarkup = new(new[]
+        {
+            new KeyboardButton[] { "Найти друзей", "Изменить анкету"},
+            new KeyboardButton[] { "Инструкция", "Удалить анкету"}
+        })
+        {
+            ResizeKeyboard = true
+        };
+        private static ReplyKeyboardMarkup replyMainMenuWithNoOrYesKeyboardMarkup = new(new[]
+        {
+            new KeyboardButton[] { "ДА", "НЕТ"}
+        })
+        {
+            ResizeKeyboard = true
+        };
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+             throw new NotImplementedException();
+        }
+    }
+}
+
+
